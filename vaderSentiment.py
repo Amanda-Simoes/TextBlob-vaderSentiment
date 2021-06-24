bc6f45bb8d0e474f804d62c70ae6e34ecfeb3d08
Amanda <amanda.simoes1707@gmail.com>
2021-06-24 08:37:40 -0300

    Finalização

--- vaderSentiment.py
+++ vaderSentiment.py
@@ -9,9 +9,10 @@ pos_correct = 0
 with io.open("positive.txt","r",encoding='latin-1') as f:
     for line in f.read().split('\n'):
         vs = analyzer.polarity_scores(line)
-        if vs['compound'] > 0:
-            pos_correct += 1
-        pos_count +=1
+        if not vs['neg'] > 0.1:
+            if vs['pos']-vs['neg'] > 0:
+                pos_correct += 1
+            pos_count +=1
 
 
 neg_count = 0
@@ -20,9 +21,10 @@ neg_correct = 0
 with io.open("negative.txt","r",encoding='latin-1') as f:
     for line in f.read().split('\n'):
         vs = analyzer.polarity_scores(line)
-        if vs['compound'] <= 0:
-            neg_correct += 1
-        neg_count +=1
+        if not vs['pos'] > 0.1:
+            if vs['pos']-vs['neg'] <= 0:
+                neg_correct += 1
+            neg_count +=1
 
 print("Positive accuracy = {}% via {} samples".format(pos_correct/pos_count*100.0, pos_count))
 print("Negative accuracy = {}% via {} samples".format(neg_correct/neg_count*100.0, neg_count))
